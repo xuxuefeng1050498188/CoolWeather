b94f2a99e9909d5eb4f09327b7680245a5b68c21
xuxuefeng <1050498188@qq.com>
2017-09-18 17:50:12 +0800

    添加部分天气布局

---
+++ CoolWeather/app/src/main/java/com/example/xuxuefeng/coolweather/activity/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.example.xuxuefeng.coolweather.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.xuxuefeng.coolweather.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
